fb1a7b225698dfcb932646ace28c34d63945eb3d
MiltonGutierrez <milton.gutierrez-l@mail.escuelaing.edu.co>
2024-12-08 23:15:31 -0500

    Prueba

--- src/main/java/com/cargomaze/cargo_maze/config/SecurityConfig.java
+++ src/main/java/com/cargomaze/cargo_maze/config/SecurityConfig.java
@@ -5,10 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private static final String[] ALLOWED_ORIGINS = {
+        "http://localhost:4200",
+        "http://localhost:8080",
+        "https://login.microsoftonline.com/ac3a534a-d5d6-42f6-aa4f-9dd5fbef911f/oauth2/v2.0/authorize",
+        "https://login.microsoftonline.com",
+        "https://calm-rock-0d4eb650f.5.azurestaticapps.net"
+    };
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -16,7 +25,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF si no es necesario para tu caso
             .cors(cors -> cors.disable()) // Deshabilitar CORS completamente
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/**").permitAll() // Rutas públicas permitidas
+                .requestMatchers("/public/**").permitAll() // Rutas públicas permitidas
                 .anyRequest().authenticated() // Cualquier otra ruta necesita autenticación
             )
             .oauth2Login(oauth2 -> oauth2
@@ -24,6 +33,21 @@ public class SecurityConfig {
                 .failureHandler((request, response, exception) -> response.sendRedirect("https://calm-rock-0d4eb650f.5.azurestaticapps.net?error=true"))
             );
     return http.build();
-}
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(ALLOWED_ORIGINS) // Permite todos los orígenes (útil solo para pruebas, no en producción)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Authorization", "Link", "X-Total-Count") // Opcional: expone encabezados adicionales
+                        .allowCredentials(true); // Cambiar a true si necesitas enviar cookies o credenciales
+            }
+        };
+    }
 
 }
