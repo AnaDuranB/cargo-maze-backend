6cba7b869dc3ec106a3b71cdb007b0236fb334d9
Ana Duran <ana.duran-b@mail.escuelaing.edu.co>
2024-12-10 23:54:03 -0500

    tests > 40% y sonar en pipeline -prueba14 creoxd

--- src/main/java/com/cargomaze/cargo_maze/services/AuthServices.java
+++ src/main/java/com/cargomaze/cargo_maze/services/AuthServices.java
@@ -2,17 +2,20 @@ package com.cargomaze.cargo_maze.services;
 
 import com.nimbusds.jwt.JWTParser;
 import com.nimbusds.jwt.SignedJWT;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthServices {
+    private static final Logger logger = LoggerFactory.getLogger(AuthServices.class);
 
     public String getEmailFromToken(String token) {
         try {
             SignedJWT signedJWT = (SignedJWT) JWTParser.parse(token);
             return signedJWT.getJWTClaimsSet().getStringClaim("email");
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Failed to parse token: {}", token, e);
             return null;
         }
     }
