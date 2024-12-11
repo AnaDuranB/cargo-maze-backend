5154a65c4716380d85f10499d706f0c971e3e11a
MiltonGutierrez <milton.gutierrez-l@mail.escuelaing.edu.co>
2024-12-11 17:45:03 -0500

    pruba cors

--- src/main/java/com/cargomaze/cargo_maze/config/SecurityConfig.java
+++ src/main/java/com/cargomaze/cargo_maze/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.cargomaze.cargo_maze.config;
 
 
+
 import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +10,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -23,6 +23,13 @@ import jakarta.servlet.http.HttpServletResponse;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private static final String[] ALLOWED_ORIGINS = {
+        "https://calm-rock-0d4eb650f.5.azurestaticapps.net",
+        "http://localhost:4200",
+        "http://localhost:8080",
+        "https://proyectoarsw.duckdns.org",
+        "https://login.microsoftonline.com"
+    };
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -31,7 +38,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(req -> req
                 .requestMatchers(HttpMethod.OPTIONS).permitAll()
-                .requestMatchers("/stompendpoint/**", "/error", "/").permitAll()
+                .requestMatchers("/stompendpoint/**", "/error","/").permitAll()
                 .requestMatchers("/cargoMaze/**").authenticated() // Requiere autenticación
             )
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
@@ -66,15 +73,17 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true); // Permitir cookies
         configuration.setAllowedOriginPatterns(Arrays.asList(
-            "http://localhost:4200/*",
-            "http://localhost:8080/*",
-            "https://calm-rock-0d4eb650f.5.azurestaticapps.net/*",
-            "https://proyectoarsw.duckdns.org/*",
-            "https://login.microsoftonline.com/*",
-            "https://cargo-maze-backend-hwgpaheeb7hreqgv.eastus2-01.azurewebsites.net/*",
-            "https://cargo-maze-backend-hwgpaheeb7hreqgv.eastus2-01.azurewebsites.net/*"
+                "http://localhost:4200/*",
+                "http://localhost:8080/*",
+                "https://calm-rock-0d4eb650f.5.azurestaticapps.net/*",
+                "https://proyectoarsw.duckdns.org/*",
+                "https://login.microsoftonline.com/*",
+                "http://localhost:4200",
+                "https://calm-rock-0d4eb650f.5.azurestaticapps.net",
+                "https://cargo-maze-backend-hwgpaheeb7hreqgv.eastus2-01.azurewebsites.net",
+                "https://cargo-maze-backend-hwgpaheeb7hreqgv.eastus2-01.azurewebsites.net/"
 
-    ));
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Métodos permitidos
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "*")); // Headers permitidos
         configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-Disposition")); // Headers expuestos
